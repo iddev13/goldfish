b5168005c9e2dcb37e12802115ffe39df2f4633d
Igor <80629173+iddev13@users.noreply.github.com>
2022-03-16 09:16:20 +0200

    1.0.1

--- server/app.js
+++ server/app.js
@@ -3,18 +3,21 @@ const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
-
+const errorHandled = require('./middleware/ErrorHandlerMiddleware')
+const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
 app.use('/api', router)
 
-// app.get('/', (req, res) => {
-// 	res.status(200).json({ message: 'WORKING!!!' })
-// })
+// Обработка ошибок всегда вызывается в самом конце, последний middleware
+app.use(errorHandled)
 
 const start = async () => {
 	try {
--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,6 +1,37 @@
+const uuid = require('uuid')
+const path = require('path')
+const { Device } = require('../models/models')
+const { nextTick } = require('process')
+const ApiError = require('../error/ApiError')
+
 class DeviceController {
 
-	async create(req, res) { }
+	async create(req, res, next) {
+		try {
+			let { name, price, brandId, typeId, info } = req.body
+			const { img } = req.files
+			let fileName = uuid.v4() + ".jpg"
+			img.mv(path.resolve(__dirname, '..', 'static', fileName))
+			const device = await Device.create({ name, price, brandId, typeId, img: fileName });
+
+			// if (info) {
+			// 	info = JSON.parse(info)
+			// 	info.forEach(i =>
+			// 		DeviceInfo.create({
+			// 			title: i.title,
+			// 			description: i.description,
+			// 			deviceId: device.id
+			// 		})
+			// 	)
+			// }
+
+			return res.json(device)
+
+		} catch (error) {
+			next(ApiError.badRequest(error.message))
+		}
+
+	}
 
 	async getAll(req, res) { }
 
